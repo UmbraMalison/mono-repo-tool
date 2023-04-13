68720aebc6072abd527242c352c77f09adef4596
Andrew Barnes <Andrew.Barnes@Renishaw.com>
2023-04-13 19:02:39 +0100

    fix indentations

--- mrt.sh
+++ mrt.sh
@@ -21,162 +21,161 @@ repos=( repo1 repo2 repo3 repo4 repo5 repo6 )
 # tags lightweight or annotated at random.
 function r_tag()
 {
-	tag="${1}"
-	annotation="${2}"
+  tag="${1}"
+  annotation="${2}"
 
-	if (( RANDOM % 2 )); then
-		# LIGHTWEIGHT tag
-		git tag "${tag}"
-	else
-		# ANNOTATED TAG
-		git tag -a "${tag}" -m "${annotation}"
-	fi
+  if (( RANDOM % 2 )); then
+    # LIGHTWEIGHT tag
+    git tag "${tag}"
+    else
+    # ANNOTATED TAG
+    git tag -a "${tag}" -m "${annotation}"
+  fi
 }
 
 function abs()
 {
-	declare -i result=$(( ${1} * ${1} ))
-	echo $(echo "sqrt(${result})" | bc)
+  declare -i result=$(( ${1} * ${1} ))
+  echo $(echo "sqrt(${result})" | bc)
 }
 
 
 function create_monorepo()
 {
-	echo
-	echo "#####################"
-	echo "# Creating MonoRepo #"
-	echo "#####################"
-	echo
+  echo
+  echo "#####################"
+  echo "# Creating MonoRepo #"
+  echo "#####################"
+  echo
 
-	rm -rf "${monorepo}"
-	mkdir -p "${monorepo}"
-	cd "${monorepo}"
+  rm -rf "${monorepo}"
+  mkdir -p "${monorepo}"
+  cd "${monorepo}"
 
-	git init
+  git init
 
-	for repo in "${repos[@]}"; do
-		mkdir -p "${repo}/src"
-		touch "${repo}/src/${repo}.file"
-		echo "Initial" > "${repo}/src/${repo}.file"
-	done
+  for repo in "${repos[@]}"; do
+    mkdir -p "${repo}/src"
+    touch "${repo}/src/${repo}.file"
+    echo "Initial" > "${repo}/src/${repo}.file"
+  done
 
-	git add --all && git commit -m "Initial Commit."
-	r_tag "v1" "version 1 - Initial."
+  git add --all && git commit -m "Initial Commit."
+  r_tag "v1" "version 1 - Initial."
 
-	# create distinct timestamps in git commit history.
-	sleep ${delay}
+  # create distinct timestamps in git commit history.
+  sleep ${delay}
 
-	# Represents a shared history of atomically logical commits, applied together.
-	changes=( "change 1" "change 2" "change 3" "change 4" "change 5" "change 6" )
+  # Represents a shared history of atomically logical commits, applied together.
+  changes=( "change 1" "change 2" "change 3" "change 4" "change 5" "change 6" )
 
-	for change in "${changes[@]}"; do
-		for repo in "${repos[@]}"; do
-			echo "${change}" >> "${repo}/src/${repo}.file"
-		done
-		git add --all && git commit -m "${change}-group."
+  for change in "${changes[@]}"; do
+    for repo in "${repos[@]}"; do
+      echo "${change}" >> "${repo}/src/${repo}.file"
+    done
+    git add --all && git commit -m "${change}-group."
 
-		# create distinct timestamps in git commit history
-		sleep ${delay}
-	done
-	r_tag "v2" "version 2 - group wide changes."
+    # create distinct timestamps in git commit history
+    sleep ${delay}
+  done
+  r_tag "v2" "version 2 - group wide changes."
 
-	# Represents repo specific commits applied before the split
-	mixed_changes=( "repo1:change 7:change 13" "repo2:change 8:change 14" "repo3:change 9:change 15" "repo4:change 10:change 16" "repo5:change 11:change 17" "repo6:change 12:change 18" )
+  # Represents repo specific commits applied before the split
+  mixed_changes=( "repo1:change 7:change 13" "repo2:change 8:change 14" "repo3:change 9:change 15" "repo4:change 10:change 16" "repo5:change 11:change 17" "repo6:change 12:change 18" )
 
-	for changes in "${mixed_changes[@]}"; do
-		readarray -d ":" -t changes < <(printf '%s' "${changes}")
-		repo="${changes[0]}"
-		unset 'changes[0]'
-		for change in "${changes[@]}"; do
-			echo "${change}" >> "${repo}/src/${repo}.file"
-			git add --all && git commit -m "${change}-${repo}."
+  for changes in "${mixed_changes[@]}"; do
+    readarray -d ":" -t changes < <(printf '%s' "${changes}")
+    repo="${changes[0]}"
+    unset 'changes[0]'
+    for change in "${changes[@]}"; do
+      echo "${change}" >> "${repo}/src/${repo}.file"
+      git add --all && git commit -m "${change}-${repo}."
 
-			# create distinct timestamps in git commit history
-			sleep ${delay}
-		done
-	done
+      # create distinct timestamps in git commit history
+      sleep ${delay}
+    done
+  done
 
-	r_tag "v3" "version 3 - repo specfic changes."
-	# monorepo complete.
+  r_tag "v3" "version 3 - repo specfic changes."
+  # monorepo complete.
 }
 
 
 function create_subrepo()
 {
-	echo
-	echo "####################"
-	echo "# Creating SubRepo #"
-	echo "####################"
-	echo
+  echo
+  echo "####################"
+  echo "# Creating SubRepo #"
+  echo "####################"
+  echo
 
-	rm -rf "${subrepo}"
-	mkdir -p "${subrepo}"
-	cd "${subrepo}"
+  rm -rf "${subrepo}"
+  mkdir -p "${subrepo}"
+  cd "${subrepo}"
 
-	for repo in "${repos[@]}"; do
-		git clone "${monorepo}" "${repo}"
-		(cd "${repo}" && export FILTER_BRANCH_SQUELCH_WARNING=1; git filter-branch --tag-name-filter cat --subdirectory-filter "${repo}")
-	done
+  for repo in "${repos[@]}"; do
+    git clone "${monorepo}" "${repo}"
+    (cd "${repo}" && export FILTER_BRANCH_SQUELCH_WARNING=1; git filter-branch --tag-name-filter cat --subdirectory-filter "${repo}")
+  done
 
 
-	# Represents atomically logical commits but split over their respective repos. These should be squashed back together...
-	changes=( "change 19" "change 20" "change 21" "change 22" "change 23" "change 24" )
+  # Represents atomically logical commits but split over their respective repos. These should be squashed back together...
+  changes=( "change 19" "change 20" "change 21" "change 22" "change 23" "change 24" )
 
-	for change in "${changes[@]}"; do
-		for repo in "${repos[@]}"; do
-			echo "${change}" >> "${repo}/src/${repo}.file"
-			(cd "${repo}" && git add --all && git commit -m "${change}-group.")
-		done
-		# create distinct timestamps in git commit history
-                sleep ${delay}
-	done
-
-	for repo in "${repos[@]}"; do
-		atag="version 4 - group wide changes applied per repo."
-		(cd "${repo}" && git tag v4)
-	done
-
-	# Represents repo specific commits applied after the split
-	mixed_changes=( "repo1:change 25:change 31" "repo2:change 26:change 32" "repo3:change 27:change 33" "repo4:change 28:change 34" "repo5:change 29:change 35" "repo6:change 30:change 36" )
-
-	for changes in "${mixed_changes[@]}"; do
-		readarray -d ":" -t changes < <(printf '%s' "${changes}")
-    repo="${changes[0]}"
-    unset 'changes[0]'
-    for change in "${changes[@]}"; do
+  for change in "${changes[@]}"; do
+    for repo in "${repos[@]}"; do
       echo "${change}" >> "${repo}/src/${repo}.file"
-      (cd "${repo}" && git add --all && git commit -m "${change}-${repo}.")
-
-			# create distinct timestamps in git commit history
-			sleep ${delay}
+      (cd "${repo}" && git add --all && git commit -m "${change}-group.")
     done
-		atag="version 5 - repo specific changes."
-		(cd "${repo}" && git tag v5)
-	done
+    # create distinct timestamps in git commit history
+    sleep ${delay}
+  done
 
-	# subrepo complete
+  for repo in "${repos[@]}"; do
+    atag="version 4 - group wide changes applied per repo."
+    (cd "${repo}" && git tag v4)
+  done
+
+  # Represents repo specific commits applied after the split
+  mixed_changes=( "repo1:change 25:change 31" "repo2:change 26:change 32" "repo3:change 27:change 33" "repo4:change 28:change 34" "repo5:change 29:change 35" "repo6:change 30:change 36" )
+
+  for changes in "${mixed_changes[@]}"; do
+    readarray -d ":" -t changes < <(printf '%s' "${changes}")
+  repo="${changes[0]}"
+  unset 'changes[0]'
+  for change in "${changes[@]}"; do
+    echo "${change}" >> "${repo}/src/${repo}.file"
+    (cd "${repo}" && git add --all && git commit -m "${change}-${repo}.")
+    # create distinct timestamps in git commit history
+    sleep ${delay}
+  done
+    atag="version 5 - repo specific changes."
+    (cd "${repo}" && git tag v5)
+  done
+
+  # subrepo complete
 
 }
 
 # This function is about making 'git-filter-repo' available to git
 function get_filter-repo()
 {
-	bin=${HOME}/.local/bin
+  bin=${HOME}/.local/bin
 
-	if [ ! -f ${bin}/git-filter-repo ]; then
+  if [ ! -f ${bin}/git-filter-repo ]; then
 
-		mkdir -p ${deps}
-		(cd  ${deps} && git clone https://github.com/newren/git-filter-repo.git)
+    mkdir -p ${deps}
+    (cd  ${deps} && git clone https://github.com/newren/git-filter-repo.git)
 
-		mkdir -p ${bin}
-		ln -s ${deps}/git-filter-repo/git-filter-repo ${bin}
+    mkdir -p ${bin}
+    ln -s ${deps}/git-filter-repo/git-filter-repo ${bin}
 
-	fi
+  fi
 
-	if [[ ":$PATH:" != *":${bin}:"* ]]; then
-		echo "Local bin missing in PATH - User might need to restart session"
-		exit
-	fi
+  if [[ ":$PATH:" != *":${bin}:"* ]]; then
+    echo "Local bin missing in PATH - User might need to restart session"
+    exit
+  fi
 }
 
 function hraban_rebase()
@@ -292,9 +291,9 @@ function synthetic()
 {
   rm -rf "${newrepo}" "${subrepo}"
 
-	#get_filter-repo # commented out for public use
-	create_monorepo
-	create_subrepo
+  #get_filter-repo # commented out for public use
+  create_monorepo
+  create_subrepo
   mkdir -p "${newrepo}"
   hraban "${subrepo}" "${newrepo}/monorepo"
   (cd "${newrepo}/monorepo" && git big-picture -o "${wdir}/monorepo1.png" && git log --oneline)
@@ -304,7 +303,7 @@ function synthetic()
 
   hraban_rebase "${newrepo}/monorepo"
   (cd "${newrepo}/monorepo" && git big-picture -o "${wdir}monorepo2.png" && git log --oneline)
-	#rebase_tags "${newrepo}"
+    #rebase_tags "${newrepo}"
 }
 
 echo "Mono Repo Tool"
@@ -315,16 +314,16 @@ echo "not written for portability or posix compliance"
 echo
 PS3="Choose which mode: "
 select opt in synthetic; do
-	case ${opt} in
-		synthetic)
-			echo "Synthetic"
-			echo "#########"
-			echo -e "\nSimulate in a controlled setup, create monorepo, split to sub repo, then convert to mono-repo."
-			synthetic
-			break
-			;;
-		*)
-			echo "Invalid"
-			;;
-	esac
+  case ${opt} in
+    synthetic)
+      echo "Synthetic"
+      echo "#########"
+      echo -e "\nSimulate in a controlled setup, create monorepo, split to sub repo, then convert to mono-repo."
+      synthetic
+      break
+      ;;
+    *)
+      echo "Invalid"
+      ;;
+  esac
 done
